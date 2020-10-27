6f533dcc6349723e2dd7730f0e751ec4fe4148c1
Alvian Zachry Faturrahman <alvianzf@gmail.com>
2020-10-27 13:10:13 +0700

    Add tests

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function warnTheSheep(queue) {
-    /// your code here
+    // your code here
+    
 }
+
+
+// Test Function do not edit
+function Test(fun, result) {
+    console.log(fun === result)
+}
+
+
+// Test assertions
+Test(warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep"]), "Oi! Sheep number 1! You are about to be eaten by a wolf!");
+Test(warnTheSheep(["sheep", "sheep", "wolf"]), "Pls go away and stop eating my sheep");
+Test(warnTheSheep(["sheep", "wolf", "sheep"]), "Oi! Sheep number 1! You are about to be eaten by a wolf!");
+Test(warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep", "sheep", "sheep"]), "Oi! Sheep number 6! You are about to be eaten by a wolf!");
